30a9d052aa74e37a274760dab51afc8e07a34ca1
adelnizamutdinov <stiggpwnz@gmail.com>
2020-10-27 21:12:00 +0300

    draft

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,41 @@
 import React from "react";
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
+import { readFileSync } from 'fs'
+import path from 'path'
+
+type DocumentFiles = {
+  sharedFiles: readonly string[];
+  pageFiles: readonly string[];
+  allFiles: readonly string[];
+}
+
+class InlineStylesHead extends Head {
+
+  getCssLinks({ sharedFiles }: DocumentFiles): JSX.Element[] {
+    const { assetPrefix } = this.context
+    return sharedFiles
+      .filter((file) => /\.css$/.test(file))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          data-href={`${assetPrefix}/_next/${file}`}
+          dangerouslySetInnerHTML={{
+            __html: readFileSync(path.join(process.cwd(), '.next', file), 'utf-8'),
+          }}
+        />
+      ))
+  }
+}
+
 // noinspection JSUnusedGlobalSymbols
 export default class MyDocument extends Document {
 
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head>
+        <InlineStylesHead>
           <link rel="apple-touch-icon" href="/icons/icon192.png" />
 
           <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon16.png" />
@@ -16,7 +44,7 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/site.webmanifest" />
 
           {process.env.THEME_COLOR && <meta name="theme-color" content={process.env.THEME_COLOR} />}
-        </Head>
+        </InlineStylesHead>
 
         <body className="dark:bg-gray-900">
           <Main />
