f40bdc6c2427ebc1c356875d292109fd956d25c4
adelnizamutdinov <stiggpwnz@gmail.com>
2020-11-28 15:02:08 +0300

    draft

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const External = ({ href, children }: PropsWithChildren<{ href: string }>) => (
 
 export default function Index(): JSX.Element {
   return (
-    <div className="prose-lg dark:prose-dark-lg">
+    <div className="prose dark:prose-dark">
       <SEO
         title="Adel Nizamutdinov"
         description="Personal Website"
