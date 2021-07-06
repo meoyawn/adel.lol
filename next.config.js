72552bb83133dea438c10c6d48319c4ae9d091a0
adelnizamutdinov <stiggpwnz@gmail.com>
2021-07-06 08:32:10 +0300

    draft

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  }
+]
+
 module.exports = {
+  poweredByHeader: false,
   images: {
     loader: 'default'
   },
   async headers() {
     return [
       {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=31536000; includeSubDomains; preload'
-      }
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
     ]
-  }
+  },
 }
