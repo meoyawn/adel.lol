23973c20f11b75ad4664392ba861dd2c2f2ff56b
adelnizamutdinov <stiggpwnz@gmail.com>
2022-07-09 13:39:26 +0400

    amen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import "../tailwind.css";
-import React from "react";
-import type { AppProps } from "next/app";
+import "../tailwind.css"
+import React from "react"
+import type { AppProps } from "next/app"
 
 // noinspection JSUnusedGlobalSymbols
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
